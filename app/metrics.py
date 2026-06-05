651a585b0046c6bd8186150d6756f8b38af35d0f
ARNAVKAWALE07 <arnavv.kawale@gmail.com>
2026-06-05 10:20:02 +0530

    fix prometheus duplicate metrics and joblib.load

--- app/metrics.py
+++ app/metrics.py
@@ -1,18 +1,29 @@
-from prometheus_client import Counter, Histogram
+from prometheus_client import Counter, Histogram, REGISTRY
 
-PREDICITON_COUNT =  Counter(
-    'fraud_prediction_total',
-    'Total predicitons',
+def get_or_create_counter(name, description, labels=[]):
+    try:
+        return Counter(name, description, labels)
+    except ValueError:
+        return REGISTRY._names_to_collectors.get(name)
+
+def get_or_create_histogram(name, description):
+    try:
+        return Histogram(name, description)
+    except ValueError:
+        return REGISTRY._names_to_collectors.get(name)
+
+PREDICTION_COUNT = get_or_create_counter(
+    'fraud_predictions_total',
+    'Total predictions',
     ['result']
 )
 
-PREDICTION_LATENCY = Histogram(
-    'fraud_prediction',
-    'prediction latency'
-
+PREDICTION_LATENCY = get_or_create_histogram(
+    'fraud_prediction_latency_seconds',
+    'Prediction latency'
 )
 
-REQUEST_COUNT = Counter(
+REQUEST_COUNT = get_or_create_counter(
     'api_requests_total',
     'Total requests',
     ['endpoint', 'status']
